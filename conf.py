5106889014b3c19a382d2c6c35d4aff4ccd170b8
thephez <thephez@users.noreply.github.com>
2023-01-17 10:33:31 -0500

    feat: project structure (#1)
    * build: add initial files from sphinx-quickstart
    
    * chore: add gitignore
    
    * chore: add more initial structure files and config
    
    * chore: newline
    
    * chore: update copyright
    
    * docs: add placeholder files
    
    * build: update config
    
    * build: add readthedocs config

---
+++ conf.py
@@ -0,0 +1,96 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Dash Core'
+copyright = '2023, Dash Core Group, Inc'
+author = 'thephez'
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+version = u'latest'
+# The full version, including alpha/beta/rc tags.
+release = u'latest'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+  'myst_parser',
+  'sphinx.ext.autodoc',
+  'sphinx_copybutton',
+  'sphinx_design',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# The master toctree document.
+master_doc = 'index'
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "pydata_sphinx_theme"
+html_static_path = ['_static']
+html_logo = 'img/dash_logo.png'
+html_css_files = [
+    'css/footer.css',
+    'css/pydata-overrides.css',
+]
+
+html_theme_options = {
+    "external_links": [
+        {"name": "User docs", "url": "https://docs.dash.org/"},
+        {"name": "Platform docs", "url": "https://dashplatform.readme.io"},
+        {"name": "Dash.org", "url": "https://www.dash.org"},
+    ],
+    "use_edit_page_button": True,
+    "github_url": "https://github.com/dashpay/docs-core",
+    "show_toc_level": 2,
+    "show_nav_level": 1,
+    "favicons": [
+      {
+         "rel": "icon",
+         "sizes": "16x16",
+         "href": "img/favicon-16x16.png",
+      },
+      {
+         "rel": "icon",
+         "sizes": "32x32",
+         "href": "img/favicon-32x32.png",
+      },
+      {
+         "rel": "icon",
+         "sizes": "96x96",
+         "href": "img/favicon-96x96.png",
+      },
+      {
+         "rel": "icon",
+         "sizes": "144x144",
+         "href": "img/favicon-144x144.png",
+      },
+   ],
+#    "navbar_start": ["navbar-logo", "languages"],
+#    "navbar_center": ["languages", "navbar-nav", "languages"],
+#    "navbar_end": ["navbar-icon-links", "version"],
+#    "secondary_sidebar_items": ["languages", "page-toc", "edit-this-page", "sourcelink"],
+#    "footer_items": ["languages", "copyright", "sphinx-version", "theme-version"],
+#   "primary_sidebar_end": ["languages"],
+}
+
+html_context = {
+    # "github_url": "https://github.com", # or your GitHub Enterprise site
+    "github_user": "dashpay",
+    "github_repo": "docs-core",
+    "github_version": "main",
+    "doc_path": "",
+}
---
+++ index.md
@@ -0,0 +1,53 @@
+```{eval-rst}
+.. meta::
+   :description: The Dash Documentation offers information and guides on Dash, the open source peer-to-peer cryptocurrency with a strong focus on the payments industry. 
+   :keywords: dash, cryptocurrency, blockchain, documentation, guide, masternodes, mining, wallets, merchants, governance, instantsend, coinjoin, privatesend, x11, instant, private, secure, digital cash
+
+.. _core-index:
+
+=========
+Core Docs
+=========
+
+Welcome to the Dash Core developer documentation. You'll find guides and documentation to help you start working with Dash Core as quickly as possible, as well as support if you get stuck. Let's jump right in!
+
+.. grid:: 1 3 3 3
+
+    .. grid-item-card:: 👤 User Docs
+        :margin: 2 2 auto auto
+        :link-type: url
+        :link: https://docs.dash.org
+        
+        Learn what Dash is and how it works. Topics include how to obtain and store Dash, the governance system, and masternode setup.
+        
+        +++
+        `Click to begin <https://docs.dash.org>`__
+
+    .. grid-item-card:: ⚙ Core Docs
+        :margin: 2 2 auto auto
+        :link-type: ref
+        :link: reference-index
+        
+        Find technical details about the Dash Core blockchain, along with protocol and API reference material.
+        
+        +++
+        :ref:`Click to begin <reference-index>`
+
+    .. grid-item-card:: 🚀 Platform Docs
+         :margin: 2 2 auto auto
+         :link-type: url
+         :link: https://dashplatform.readme.io
+
+         Start working with Dash Platform and discover how you can use its powerful capabilities to power your Web3 project.
+         
+         +++
+         `Click to begin <https://dashplatform.readme.io>`__
+
+.. _core-reference:
+
+.. toctree::
+   :maxdepth: 2
+   :titlesonly:
+
+   docs/ref/core-ref-introduction
+```
